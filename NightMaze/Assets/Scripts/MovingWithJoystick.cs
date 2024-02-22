0b38ab090c5fdcc4e0b0e9d363b40f401d204edc
Daniyl <user@gmail.com>
2024-02-22 20:23:47 +0200

    lights

--- NightMaze/Assets/Scripts/MovingWithJoystick.cs
+++ NightMaze/Assets/Scripts/MovingWithJoystick.cs
@@ -9,7 +9,26 @@ public class MovingWithJoystick : MonoBehaviour
     [SerializeField] private FixedJoystick _joystick;
     [SerializeField] private Animator _animator;
 
+
     [SerializeField] private float _moveSpeed;
+
+    private void Update()
+    {
+        _animator.SetFloat("Speed", Mathf.Abs(_joystick.Horizontal) + Mathf.Abs(_joystick.Vertical));
+
+
+        if (_joystick.Horizontal > 0)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+            //transform.Rotate(0, 0, 0);
+        }
+        if (_joystick.Horizontal < 0)
+        {
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+            //transform.Rotate(0, 180, 0);
+        }
+    }
+
     private void FixedUpdate()
     {
         _rigidbody.velocity = new Vector2(_joystick.Horizontal * _moveSpeed, _joystick.Vertical * _moveSpeed);
