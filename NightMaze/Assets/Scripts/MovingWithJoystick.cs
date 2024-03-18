ef5ab0cc43b117391fddeb13fb1407f199389b4d
Danya <user@gmail.com>
2024-03-18 10:07:19 +0200

    QQ-54 <jump features>

--- NightMaze/Assets/Scripts/MovingWithJoystick.cs
+++ NightMaze/Assets/Scripts/MovingWithJoystick.cs
@@ -1,6 +1,7 @@
 using TMPro;
 using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 [RequireComponent (typeof(Rigidbody2D), typeof(BoxCollider2D))]
 public class MovingWithJoystick : MonoBehaviour
@@ -11,9 +12,14 @@ public class MovingWithJoystick : MonoBehaviour
     [SerializeField] private FloatingJoystick joystick;
     [SerializeField] private Animator animator;
     [SerializeField] private float moveSpeed;
+    [SerializeField] private Slider slider;
+    [SerializeField] private Button sliderButton;
+    [SerializeField] private TMP_Text sliderText;
 
     [SerializeField] private float teleportSpeed; // Швидкість телепортації
     [SerializeField] private bool isJumping = false; // прапорець, що показує, чи рухається персонаж
+    [SerializeField] private bool jumpIsAllowed;
+    private int numberOfJumps = 0;
     private Vector2 targetPosition; // Позиція, на яку буде телепортуватися персонаж
 
     private float angle;
@@ -28,19 +34,21 @@ public class MovingWithJoystick : MonoBehaviour
         checkBoxSize = new Vector2(0.04f, 0.04f);
         wallLayer = LayerMask.GetMask("Wall");
         halfWallLayer = LayerMask.GetMask("HalfWall");
+        jumpIsAllowed = false;
+        slider.value = 0;
     }
     public void OnButtonClick()
     {
         if (!isJumping)
         {
             Vector2 playerPosition = transform.position;
-            Vector2 nextTile = new Vector2(playerPosition.x + lastJoystickDirection.x, playerPosition.y + lastJoystickDirection.y);
             Vector2 nextTileToNext = new Vector2(playerPosition.x + 2 * lastJoystickDirection.x, playerPosition.y + 2 * lastJoystickDirection.y);
-            if (Physics2D.OverlapBox(nextTile, checkBoxSize, angle, halfWallLayer) != null && Physics2D.OverlapBox(nextTileToNext, checkBoxSize, 0, wallLayer) == null && Physics2D.OverlapBox(nextTileToNext, checkBoxSize, 0, halfWallLayer) == null)
+            if (jumpIsAllowed && numberOfJumps > 0)
             {
                 targetPosition = nextTileToNext;
                 dust.Play();
                 capsuleCollider.isTrigger = true;
+                numberOfJumps--;
                 StartCoroutine(MoveToTarget()); // запуск корутини для поступового руху
             }
         }
@@ -48,7 +56,6 @@ public class MovingWithJoystick : MonoBehaviour
     private void Update()
     {
         animator.SetFloat("Speed", Mathf.Abs(joystick.Horizontal) + Mathf.Abs(joystick.Vertical));
-
         // Оберт персонажа
         if (joystick.Horizontal > 0)
         {
@@ -62,6 +69,20 @@ public class MovingWithJoystick : MonoBehaviour
         {
             angle = Mathf.Atan2(joystick.Vertical, joystick.Horizontal) * Mathf.Rad2Deg;
             lastJoystickDirection = new Vector2(joystick.Horizontal, joystick.Vertical);
+            Vector2 playerPosition = transform.position;
+            Vector2 nextTile = new Vector2(playerPosition.x + lastJoystickDirection.x, playerPosition.y + lastJoystickDirection.y);
+            Vector2 nextTileToNext = new Vector2(playerPosition.x + 2 * lastJoystickDirection.x, playerPosition.y + 2 * lastJoystickDirection.y);
+            jumpIsAllowed = Physics2D.OverlapBox(nextTile, checkBoxSize, angle, halfWallLayer) != null && Physics2D.OverlapBox(nextTileToNext, checkBoxSize, 0, wallLayer) == null && Physics2D.OverlapBox(nextTileToNext, checkBoxSize, 0, halfWallLayer) == null;
+        }
+
+        sliderText.text = numberOfJumps.ToString();
+        if (jumpIsAllowed)
+        {
+            sliderButton.interactable = true;
+        }
+        else
+        {
+            sliderButton.interactable = false;
         }
         if (Input.GetKeyDown(KeyCode.Space))
         {
@@ -75,7 +96,16 @@ public class MovingWithJoystick : MonoBehaviour
     }
     private void FixedUpdate()
     {
-        if(!isJumping)
+        if (slider.value < 1 && numberOfJumps < 2)
+        {
+            slider.value += 0.012f;
+        }
+        else if (slider.value >= 1)
+        {
+            slider.value = 0;
+            numberOfJumps++;
+        }
+        if (!isJumping)
         {
             rigidbody2d.velocity = new Vector2(joystick.Horizontal * moveSpeed, joystick.Vertical * moveSpeed);
         }
